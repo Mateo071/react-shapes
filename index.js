e39cf7e557bf3455b0fe343355ed37cd1a185b75
Mateo071 <floresmateo2000@gmail.com>
2023-07-25 16:49:09 -0400

    fixed colors

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const container = document.querySelector(`#root`);
 
 const App = () => {
-  return (Circle, Square)
+  return (
+    <section>
+      <Circle />
+      <Circle />
+      <Circle />
+      <Square />
+      <Square />
+      <Square />
+    </section>
+    
+  )
 }
 
 const Circle = () => {
   return (
-    <section id="circle"></section>
+    <section id="circle" className={RandomColor()}></section>
   )
 }
 
 const Square = () => {
   return (
-    <section id="square"></section>
+    <section id="square" className={RandomColor()}></section>
   )
 }
 
-const root = createRoot(container);
+const RandomColor = () => {
+  const colors = [`red`, `blue`, `green`, `yellow`, `pink`]
+
+  return (colors[Math.floor(Math.random()) * colors.length])
+}
+
+const root = ReactDOM.createRoot(container);
 root.render(<App />);
