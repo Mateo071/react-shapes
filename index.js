a1cc73b5f4950ef2c793973f8e1b8b70cd6e1e9f
Mateo071 <floresmateo2000@gmail.com>
2023-07-25 16:14:29 -0400

    first attempt

---
+++ index.js
@@ -0,0 +1,20 @@
+const container = document.querySelector(`#root`);
+
+const App = () => {
+  return (Circle, Square)
+}
+
+const Circle = () => {
+  return (
+    <section id="circle"></section>
+  )
+}
+
+const Square = () => {
+  return (
+    <section id="square"></section>
+  )
+}
+
+const root = createRoot(container);
+root.render(<App />);
